bd81d8e252d033a3f65bb78b9828901e2bc711b6
Aaron Graham <grahamam@ornl.gov>
2019-02-04 12:23:11 -0500

    Updates to edit_nodal.py
    print TH, transient, Xenon/Samarium, and corner point flux values.
    Also fixes bugs in boundary conditions and symmetry.

--- scripts/postprocessing/edit_nodal.py
+++ scripts/postprocessing/edit_nodal.py
@@ -5,6 +5,23 @@ import h5py
 import numpy as np
 import pylab as plt
 
+def assembliesAreSplit(sym, coremap):
+    splitx = False
+    splity = False
+    if sym == 4:
+        if np.mod(coremap.shape[0],2) == 1:
+            splitx = True
+        if np.mod(coremap.shape[1],2) == 1:
+            splity = True
+    return [splitx, splity]
+
+def K2F(data):
+    for node in xrange(data.shape[0]):
+        for level in xrange(data.shape[1]):
+            for asy in xrange(data.shape[2]):
+                if data[node,level,asy] > np.finfo(float).eps:
+                    data[node,level,asy] = (data[node,level,asy]-273.15) * 1.8 + 32
+
 if len(sys.argv) > 1:
   h5=h5py.File(sys.argv[1])
 else:
@@ -89,6 +106,17 @@ for i in range(len(nodal_axial_mesh)):
     break
 print "Fuel Start Axial Level: " + str(fuelstartz)
 print "Fuel  Stop Axial Level: " + str(fuelstopz)
+
+# Print the number of nodes in x an dy directions
+[splitx, splity] = assembliesAreSplit(sym, coremap)
+nx = nodal_coremap.shape[0]*2
+ny = nodal_coremap.shape[1]*2
+if splitx:
+    nx = nx - 1
+if splity:
+    ny = ny - 1
+print "Number of nodes and widths in x direction: " + str(nx) + ' ' + str(A)
+print "Number of nodes and widths in y direction: " + str(ny) + ' ' + str(A)
 print ""
 
 # Get array and define function to go to fuel assembly index
@@ -109,6 +137,7 @@ while True:
     if not stateStr in h5:
       break
 
+    # Read all the state data
     keff=h5[stateStr + '/keff'].value
     ppw=h5[stateStr + '/pin_powers'].value
     cur=h5[stateStr + '/NODAL_XS/CUR'].value
@@ -120,43 +149,120 @@ while True:
     kxf=h5[stateStr + '/NODAL_XS/KXSF'].value
     xsr=h5[stateStr + '/NODAL_XS/XSRM'].value
     xstr=h5[stateStr + '/NODAL_XS/XSTR'].value
-    xss=h5[stateStr + '/NODAL_XS/XSS'].value
+    xss=h5[stateStr + '/NODAL_XS/XSS'].value #First two axes are [from, to]
+    swcpv=h5[stateStr + '/NODAL_XS/CORNERFLUX'].value
+    rhowrefv=h5[stateStr + '/NODAL_XS/COOLDENS'].value # g/cc
+    tfrefv=h5[stateStr + '/NODAL_XS/FUELTEMP'].value # K
+    tfsrefv=h5[stateStr + '/NODAL_XS/FUELSURFTEMP'].value # K
+    tcolrefv=h5[stateStr + '/NODAL_XS/COOLTEMP'].value # K
+    wtfrrov=h5[stateStr + '/NODAL_XS/WETVOLFRAC'].value
+    fufrv=h5[stateStr + '/NODAL_XS/FUELVOLFRAC'].value
+    dfiv=h5[stateStr + '/NODAL_XS/I135ND'].value # 1/barn-cm
+    dpmv=h5[stateStr + '/NODAL_XS/PM149ND'].value # 1/barn-cm
+    dxev=h5[stateStr + '/NODAL_XS/SM149ND'].value # 1/barn-cm
+    dsmv=h5[stateStr + '/NODAL_XS/XE135ND'].value # 1/barn-cm
+    xxeabv=h5[stateStr + '/NODAL_XS/XE135XSAB'].value # barns
+    xsmabv=h5[stateStr + '/NODAL_XS/SM149XSAB'].value # barns
+    xhidmiv=h5[stateStr + '/NODAL_XS/CHID'].value
+    betav=h5[stateStr + '/NODAL_XS/BETA'].value
+    velocv=h5[stateStr + '/NODAL_XS/VELOCITY'].value # cm/sec
+
+    # Before we do anything convert temperatures from K to F
+    print 'test 0'
+    K2F(tfrefv)
+    K2F(tfsrefv)
+    K2F(tcolrefv)
 
     print "STATE {0:4d}".format(st)
     print "-------------------------------------------------------"
     print "  k-eff: {0:7.5f}".format(keff)
-    nasy=flx.shape[0]
-    nz=flx.shape[1]
+    nasy=flx.shape[3]
+    nz=flx.shape[2]
     for az in xrange(nz):
         print "  AXIAL LEVEL {0:4d}".format(az+1)
         print ""
 
         for asy in xrange(nasy):
             powasy = asy2powasy(asy+1) - 1
+
+            # Print assembly header
             print "    ASSEMBLY {0:4d}".format(asy+1)
+
+            # Print basic fluxes, cross sections, and balance
             print "        FLUX                  XSTR                  XSRM                  XSF                   NXSF                  KXSF                  CHI                   XSS                    Balance"
             print "  node  1          2          1          2          1          2          1          2          1          2          1          2          1          2          1>2        2>1         1           2"
             for nd in xrange(4):
-                col=np.multiply(xsr[asy,0,nd,:],flx[asy,0,nd,:])
-                scat=np.transpose(xss[asy,0,nd,:,:]); scat[0,0]=0.0; scat[1,1]=0.0
-                ssrc=np.dot(scat,flx[asy,0,nd,:])
-                fsrc=chi[asy,0,nd,:]/keff*np.dot(nxf[asy,0,nd,:],flx[asy,0,nd,:])
-                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e} {9:10.4e} {10:10.4e} {11:10.4e} {12:10.4e} {13:10.4e} {14:10.4e} {15:10.4e} {16:10.4e} {17:11.4e} {18:11.4e}".format(nd+1,flx[asy,az,nd,0],flx[asy,az,nd,1],xstr[asy,az,nd,0],xstr[asy,az,nd,1],xsr[asy,az,nd,0],xsr[asy,az,nd,1],xf[asy,az,nd,0],xf[asy,az,nd,1],nxf[asy,az,nd,0],nxf[asy,az,nd,1],kxf[asy,az,nd,0],kxf[asy,az,nd,1],chi[asy,az,nd,0],chi[asy,az,nd,1],xss[asy,az,nd,0,1],xss[asy,az,nd,1,0],(-col[0]+ssrc[0]+fsrc[0])*V-np.sum(cur[asy,az,nd,0])*A,(-col[1]+ssrc[1]+fsrc[1])*V-np.sum(cur[asy,az,nd,1])*A)
+                col=np.multiply(xsr[:,nd,az,asy],flx[:,nd,az,asy])
+                scat=np.transpose(xss[:,:,nd,az,asy]); scat[0,0]=0.0; scat[1,1]=0.0
+                ssrc=np.dot(scat,flx[:,nd,az,asy])
+                fsrc=chi[:,nd,az,asy]/keff*np.dot(nxf[:,nd,az,asy],flx[:,nd,az,asy])
+                # Fluxes and cross sections
+                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e} {9:10.4e} {10:10.4e} {11:10.4e} {12:10.4e} {13:10.4e} {14:10.4e} {15:10.4e} {16:10.4e} {17:11.4e} {18:11.4e}".format(\
+                    nd+1, flx[0,nd,az,asy], flx[1,nd,az,asy], xstr[0,nd,az,asy], xstr[1,nd,az,asy], xsr[0,nd,az,asy], xsr[1,nd,az,asy], xf[0,nd,az,asy], xf[1,nd,az,asy], nxf[0,nd,az,asy], nxf[1,nd,az,asy], kxf[0,nd,az,asy], kxf[1,nd,az,asy], \
+                    chi[0,nd,az,asy], chi[1,nd,az,asy], xss[0,1,nd,az,asy], xss[1,0,nd,az,asy], (-col[0]+ssrc[0]+fsrc[0])*V-np.sum(cur[:,0,nd,az,asy])*A, (-col[1]+ssrc[1]+fsrc[1])*V-np.sum(cur[:,1,nd,az,asy])*A)
             print ""
+
+            # Currents
             print "Current"
             print "        WEST                  NORTH                 EAST                  SOUTH                 TOP                   BOTTOM"
             print "  node  1          2          1          2          1          2          1          2          1          2          1          2"
             for nd in xrange(4):
-                print "  {0:1d}    {1:10.3e} {2:10.3e} {3:10.3e} {4:10.3e} {5:10.3e} {6:10.3e} {7:10.3e} {8:10.3e} {9:10.3e} {10:10.3e} {9:11.3e} {12:10.3e}".format(nd+1,cur[asy,az,nd,0,0],cur[asy,az,nd,1,0],cur[asy,az,nd,0,1],cur[asy,az,nd,1,1],cur[asy,az,nd,0,2],cur[asy,az,nd,1,2],cur[asy,az,nd,0,3],cur[asy,az,nd,1,3],cur[asy,az,nd,0,4],cur[asy,az,nd,1,4],cur[asy,az,nd,0,5],cur[asy,az,nd,1,5])
+                print "  {0:1d}    {1:10.3e} {2:10.3e} {3:10.3e} {4:10.3e} {5:10.3e} {6:10.3e} {7:10.3e} {8:10.3e} {9:10.3e} {10:10.3e} {9:11.3e} {12:10.3e}".format(\
+                    nd+1, cur[0,0,nd,az,asy], cur[0,1,nd,az,asy], cur[1,0,nd,az,asy], cur[1,1,nd,az,asy], cur[2,0,nd,az,asy], cur[2,1,nd,az,asy], cur[3,0,nd,az,asy], \
+                    cur[3,1,nd,az,asy], cur[4,0,nd,az,asy], cur[4,1,nd,az,asy], cur[5,0,nd,az,asy], cur[5,1,nd,az,asy])
             print ""
+
+            # Surface Fluxes
             print "Surface Flux"
             print "        WEST                  NORTH                 EAST                  SOUTH                 TOP                   BOTTOM"
             print "  node  1          2          1          2          1          2          1          2          1          2          1          2"
             for nd in xrange(4):
-                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e} {9:10.4e} {10:10.4e} {9:11.4e} {12:10.4e}".format(nd+1,sfx[asy,az,nd,0,0],sfx[asy,az,nd,1,0],sfx[asy,az,nd,0,1],sfx[asy,az,nd,1,1],sfx[asy,az,nd,0,2],sfx[asy,az,nd,1,2],sfx[asy,az,nd,0,3],sfx[asy,az,nd,1,3],sfx[asy,az,nd,0,4],sfx[asy,az,nd,1,4],sfx[asy,az,nd,0,5],sfx[asy,az,nd,1,5])
+                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e} {9:10.4e} {10:10.4e} {9:11.4e} {12:10.4e}".format(\
+                    nd+1, sfx[0,0,nd,az,asy], sfx[0,1,nd,az,asy], sfx[1,0,nd,az,asy], sfx[1,1,nd,az,asy], sfx[2,0,nd,az,asy], sfx[2,1,nd,az,asy], sfx[3,0,nd,az,asy], \
+                    sfx[3,1,nd,az,asy], sfx[4,0,nd,az,asy], sfx[4,1,nd,az,asy], sfx[5,0,nd,az,asy], sfx[5,1,nd,az,asy])
             print ""
+
+            # Pin Powers
             if powasy >= 0 and az >= fuelstartz-1 and az <= fuelstopz-1:
                 print "Pin Powers"
                 for i in xrange(ppw.shape[0]):
                     print '      '+' '.join('%6.4f' % ppw[i,j,az-fuelstartz,powasy] for j in xrange(ppw.shape[1]))
                 print ""
+
+            # TH Data
+            print "TH"
+            print "  node  RHOWREFV   TCOLREFV   TFREFV     TFSREFV    WTFRROV    FUFRV"
+            for nd in xrange(4):
+                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e}".format(\
+                    nd+1, rhowrefv[nd,az,asy], tcolrefv[nd,az,asy], tfrefv[nd,az,asy], tfsrefv[nd,az,asy], wtfrrov[nd,az,asy], fufrv[nd,az,asy])
+            print ""
+
+            # Transient
+            print "Transient"
+            print "        VELOCV                XHIDMIV               BETAV"
+            print "  node  1          2          1          2          1          2          3          4          5          6"
+            for nd in xrange(4):
+                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e} {9:10.4e} {10:10.4e}".format(\
+                    nd+1, velocv[0,nd,az,asy], velocv[1,nd,az,asy], xhidmiv[0,nd,az,asy], xhidmiv[1,nd,az,asy], betav[0,nd,az,asy], betav[1,nd,az,asy], \
+                    betav[2,nd,az,asy], betav[3,nd,az,asy], betav[4,nd,az,asy], betav[5,nd,az,asy])
+            print ""
+
+            # Xe/Sm
+            print "Xenon/Samarium"
+            print "        DFIV       DXEV       DPMV       DSMV       XXEABV                XSMABV"
+            print "  node                                              1          2          1          2"
+            for nd in xrange(4):
+                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e}".format(\
+                    nd+1, dfiv[nd,az,asy], dxev[nd,az,asy], dpmv[nd,az,asy], dsmv[nd,az,asy], xxeabv[0,nd,az,asy], xxeabv[1,nd,az,asy], \
+                    xsmabv[0,nd,az,asy], xsmabv[1,nd,az,asy])
+            print ""
+
+            # Corner point fluxes
+            print "SWCPV"
+            print "        NORTHWEST             NORTHEAST             SOUTHWEST             SOUTHEAST"
+            print "  node  1          2          1          2          1          2          1          2"
+            for nd in xrange(4):
+                print "  {0:1d}     {1:10.4e} {2:10.4e} {3:10.4e} {4:10.4e} {5:10.4e} {6:10.4e} {7:10.4e} {8:10.4e}".format(\
+                    nd+1, swcpv[0,0,nd,az,asy], swcpv[0,1,nd,az,asy], swcpv[1,0,nd,az,asy], swcpv[1,1,nd,az,asy], swcpv[2,0,nd,az,asy], \
+                    swcpv[2,1,nd,az,asy], swcpv[3,0,nd,az,asy], swcpv[3,1,nd,az,asy])
+            print ""
