d0893cdd134abfb9a79e18dc1367e01289656914
Aaron Graham <grahamam@ornl.gov>
2019-10-09 12:46:48 -0400

    Add gprof_calls_vis.sh to script generation of png from gprof output

---
+++ scripts/code_analysis/gprof_calls_vis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run the executable compilied with gprof first
+exe=$1
+basename=$2
+shift; shift
+if [ $# -eq 1 ]
+then
+  copyname=$1
+  cp $copyname $basename.out
+fi
+
+gprof $exe $basename.out > $basename.gprof
+gprof2dot $basename.gprof > $basename.dot
+dot $basename.dot -Tpng -o $basename.png
+# visualize on ubuntu with eog
