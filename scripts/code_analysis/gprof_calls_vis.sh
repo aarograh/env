4c19d51d86b114a98d0e251782921be8066db656
Aaron Graham <grahamam@ornl.gov>
2022-02-09 16:05:52 -0500

    Cleanup gprof_calls_vis.sh

--- scripts/code_analysis/gprof_calls_vis.sh
+++ scripts/code_analysis/gprof_calls_vis.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Run the executable compilied with gprof first
 exe=$1
 basename=$2
 shift; shift
-if [ $# -eq 1 ]
-then
-  copyname=$1
-  cp $copyname $basename.out
-fi
 
-gprof $exe $basename.out.* > $basename.gprof
+gprof $exe $basename* > $basename.gprof
 gprof2dot $basename.gprof > $basename.dot
 dot $basename.dot -Tpng -o $basename.png
 # visualize on ubuntu with eog
