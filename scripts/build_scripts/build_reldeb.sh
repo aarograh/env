856d2955a54896cb885004b162ef522806643178
Aaron Graham <grahamam@ornl.gov>
2023-07-24 09:47:37 -0400

    Cache script updates

--- scripts/build_scripts/build_reldeb.sh
+++ scripts/build_scripts/build_reldeb.sh
@@ -4,5 +4,6 @@ SOURCE_DIR="$1"
 shift
 
 command="$SOURCE_DIR/build_scripts/build_with_casl_env_830_newTPLs.sh $SOURCE_DIR/build_scripts/build_with_debug_info.sh $SOURCE_DIR/build_scripts/build_petsc_release_omp_all.sh $SOURCE_DIR -DFutility_ENABLE_DBC=ON -DMPACT_ENABLE_MEMPROF=ON -DMPACT_TEST_CATEGORIES=HEAVY $@"
+#command="$SOURCE_DIR/build_scripts/build_with_casl_env_2023-04-05.sh $SOURCE_DIR/build_scripts/build_with_debug_info.sh $SOURCE_DIR/build_scripts/build_petsc_release_omp_all.sh $SOURCE_DIR -DFutility_ENABLE_DBC=ON -DMPACT_ENABLE_MEMPROF=ON -DMPACT_TEST_CATEGORIES=HEAVY $@"
 echo $command
 $command
