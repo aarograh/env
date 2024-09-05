5041044a2cbe71313e369557b1bc9f1ab6ffd357
Aaron M. Graham <aaron.graham@veracitynuclear.com>
2024-09-05 11:23:43 -0400

    Update mpact spack build script

--- scripts/build_scripts/build_mpact_from_spack.sh
+++ scripts/build_scripts/build_mpact_from_spack.sh
@@ -3,7 +3,7 @@ SCRIPT=$1
 MPACT=$2
 shift 2
 
-SPACK_DIR=~/spack/var/spack/environments/vera_4-4_tpls/.spack-env/view
+SPACK_DIR=${SPACK_ENV}/.spack-env/view
 SPACK_LIBS=${SPACK_DIR}/lib
 SPACK_INCLUDES=${SPACK_DIR}/include
 
@@ -14,13 +14,15 @@ ${SCRIPT} ${MPACT}        -DMPACT_BUILD_STANDARD=OFF                          \
    -DTPL_ENABLE_BLAS=ON   -DBLAS_LIBRARY_DIRS:FILENAME=${SPACK_LIBS}          \
    -DTPL_ENABLE_LAPACK=ON -DLAPACK_LIBRARY_DIRS:FILEPATH=${SPACK_LIBS}        \
    -DTPL_ENABLE_HDF5=ON   -DHDF5_LIBRARY_DIRS:FILEPATH=${SPACK_LIBS}          \
-                          -DTPL_HDF5_INCLUDE_DIRS:FILEPATH="${SPACK_INCLUDES};${SPACK_DIR}/mod/shared"      \
+                          -DTPL_HDF5_INCLUDE_DIRS:FILEPATH="${SPACK_INCLUDES};${SPACK_INCLUDES}/shared"      \
                           -DHDF5_LIBRARY_NAMES:STRING=${HDF5_LIBRARY_NAMES}   \
    -DTPL_ENABLE_SILO=ON   -DSILO_LIBRARY_DIRS:FILEPATH=${SPACK_LIBS}          \
                           -DSILO_INCLUDE_DIRS:FILEPATH=${SPACK_INCLUDES}      \
                           -DPETSC_LIBRARY_DIRS:FILEPATH=${SPACK_LIBS}         \
                           -DPETSC_INCLUDE_DIRS:FILEPATH=${SPACK_INCLUDES}     \
                           -DPETSC_LIBRARY_NAMES:STRING=${PETSC_LIBRARY_NAMES} \
+                          -DMPI_EXEC_MAX_NUMPROCS=16 \
+   -DMPACT_ENABLE_PolarisMoonraker=ON \
    $@
 #   -DTPL_ENABLE_SLEPC=ON  -DSLEPC_LIBRARY_DIRS:FILEPATH=${SLEPC_LIBRARY_DIRS}    \
 #                          -DSLEPC_INCLUDE_DIRS:FILEPATH=${SLEPC_INCLUDE_DIRS}    \
